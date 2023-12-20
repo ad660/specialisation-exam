a22d430949cfc53be87fcbec292d2c5afecf300e
Ether <ayanahennings@gmail.com>
2023-12-20 20:42:37 +0000

    pull req

--- hashing.py
+++ hashing.py
@@ -7,6 +7,8 @@ the title.
 
 To obtain this code, the librarian uses a method that qualifies each both using a method that labels them as though they
 are ASCII values and add up the values of the letters in the title.
-If there's no collision then the book will be placed on an empty shelf if its title code points to it and of
-the title code points ot an occupied shelf the librarian will have to recalculate where to shelve it.
+If there's no collision then the book will be placed on an empty shelf and if its title code points an occupied shelf the
+librarian will have to recalculate where to shelve it.
+
+
 """
